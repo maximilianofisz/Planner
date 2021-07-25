ada837c3b0ae8be0944449d35ea5c923c284e267
maximilianofisz <80064716+maximilianofisz@users.noreply.github.com>
2021-07-25 12:07:05 -0300

    Created a Helper class to make queries easier to execute and Model. Added a ViewModel which will probably used to test section of app (daily tasks)

---
+++ Helpers/DbHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Planner.Helpers
+{
+    public class DbHelper
+    {
+        public static SqlConnection GetConnection()
+        {
+            return new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=PlannerDB;");
+        }
+    }
+}
---
+++ Models/ToDoItem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Planner.Models
+{
+    public class ToDoItem
+    {
+        public int Id { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public bool IsDone { get; set; }
+
+        public bool IsRecurring { get; set; }
+
+        [Required]
+        [MinLength(3, ErrorMessage = "Be more descriptive, please")]
+        [MaxLength(200, ErrorMessage = "Geez, be more concise, please")]
+        public string Title { get; set; }
+
+
+    }
+}
