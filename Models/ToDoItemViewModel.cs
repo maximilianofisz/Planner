5df184ea664155a7c189c26902da4bdb73ed77d3
maximilianofisz <80064716+maximilianofisz@users.noreply.github.com>
2021-08-10 23:41:13 -0300

    Added History View. Added link to enter History page. Added link to return to Home from History. Added Actions to Home Controller for the history functions (Display and Clear History). Added method to ViewModel to execute query and clear history. Switched main query for a stored procedure.

--- Models/ToDoItemViewModel.cs
+++ Models/ToDoItemViewModel.cs
@@ -5,7 +5,9 @@ using System.Threading.Tasks;
 using Planner.Helpers;
 using Planner.Models;
 using Dapper;
+using Dapper.Contrib;
 using System.Data.SqlClient;
+using System.Data;
 
 namespace Planner.Models
 {
@@ -16,7 +18,8 @@ namespace Planner.Models
             using (var db = DbHelper.GetConnection())
             {
                 this.EditableItem = new ToDoItem();
-                this.ToDoItems = db.Query<ToDoItem>("SELECT * FROM toDo ORDER BY Date ASC").ToList();
+                //this.ToDoItems = db.Query<ToDoItem>("SELECT * FROM toDo ORDER BY Date ASC").ToList();
+                this.ToDoItems = db.Query<ToDoItem>("GetAllData", null, null, true, null, CommandType.StoredProcedure).ToList();
             }
         }
 
@@ -24,6 +27,14 @@ namespace Planner.Models
 
         public List<ToDoItem> ToDoItems { get; set; }
 
-
+        public void ClearHistory()
+        {
+            using (var db = DbHelper.GetConnection())
+            {
+                var parameters = new DynamicParameters();
+                parameters.Add("@YesterdaysDate", DateTime.Now.AddDays(-1));
+                db.Execute("ClearHistory", parameters, null, null, CommandType.StoredProcedure);
+            }
+        }
     }
 }
